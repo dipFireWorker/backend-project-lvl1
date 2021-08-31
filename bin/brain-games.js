4ef42205dadb17266b682f7ea4d37ea8f9f548ae
Timur Anvartdinov <AnvartdinovTimur@gmail.com>
2021-08-31 16:31:22 +0300

    fix problems with brain-even

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable import/no-named-as-default */
 /* eslint-disable import/extensions */
 /* eslint-disable no-console */
 
