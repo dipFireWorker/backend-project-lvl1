2dd10f8e44458d2f6b86f190a6534fd57abc3edc
Timur Anvartdinov <AnvartdinovTimur@gmail.com>
2021-08-31 15:40:52 +0300

    add brain-even

---
+++ bin/brain-even.js
@@ -0,0 +1,28 @@
+import readlineSync from 'readline-sync';
+/* eslint-disable no-console */
+
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+const isEven = (number) => (number % 2 === 0 ? 'yes' : 'no');
+
+export default () => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  for (let i = 0; i < 3; i += 1) {
+    const number = getRandomInt(1, 100);
+    console.log(`Question: ${number}`);
+
+    const answer = readlineSync.question('Question: ');
+    if (isEven(number) === answer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'&{answer}' is wrong answer ;(. Correct answer was '${isEven(number)}'.`);
+      console.log("Let's try again, Bill!");
+      break;
+    }
+
+    if (i === 2) {
+      console.log('Congratulations, Bill!');
+    }
+  }
+};
